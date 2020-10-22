8bb64077fab1f5b1bf4bafbc125aa9a37303325c
Elena Shashina <elvehnn@yandex.ru>
2020-10-22 23:46:42 +0300

    feat: completed countCats and dream-team tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || members.length === 0) {
+    return false;
+  };
+
+  let arrNew = [];
+  let str;
+  for (let i = 0; i < members.length; i++) {
+   if (typeof members[i] === "string") {
+    str = members[i].trim();
+    arrNew.push((str[0]).toUpperCase()); 
+    };
+  }; 
+   
+  return arrNew.sort().join('');
 };
