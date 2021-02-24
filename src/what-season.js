a6439ba88aaea6891485e3a16f2a3b3d60c4d77b
Elena Shashina <elvehnn@yandex.ru>
2021-02-24 23:20:54 +0300

    feat: Hanoi tower and What season done

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(d) {
+  if (!d) {
+    return ('Unable to determine the time of year!');
+  } else if (Object.prototype.toString.call(d) !== "[object Date]") {
+    console.log(Object.prototype.toString.call(d));
+    throw new Error ("Error");
+  } else {
+    let month = d.getMonth();
+    if (month === 0 || month === 1 || month === 11) {
+        return 'winter';
+    } else if (month === 2 || month === 3 || month === 4) {
+        return 'spring';
+    } else if (month === 5 || month === 6 || month === 7) {
+        return 'summer';
+    } else {
+        return 'autumn';
+    };
+  };
 };
