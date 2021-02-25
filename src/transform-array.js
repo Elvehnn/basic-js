b67813247e4a7c440f7f0fc9ee6d22870f84437e
Elena Shashina <elvehnn@yandex.ru>
2021-02-25 18:02:17 +0300

    feat: completed task Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!arr) {
+    throw new Error ("Error");
+  } else if (arr === []) {
+    return arr;
+  };
+  
+  const cloneArr = arr.slice();
+  const resArr = [];
+  const controlLines = ['--discard-next','--discard-prev','--double-next','--double-prev'];
+  const empty = 'empty';
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    let currItem = cloneArr[i];
+    if (!(controlLines.includes(currItem) || controlLines.includes(empty))) {
+      resArr.push(currItem);
+      continue;
+    } else {
+        switch(currItem) {
+          case '--discard-next':
+            if (i < arr.length - 1) {
+              cloneArr[i + 1] = empty;
+              i++;
+            }
+            break;
+
+          case '--discard-prev':
+              if (!((i === 0) || cloneArr[i - 1] === empty)) {
+                resArr.pop();
+              }
+              break;
+
+          case '--double-next':
+              if (i < arr.length - 1) {
+                resArr.push(cloneArr[i + 1]);
+              }
+              break;    
+
+          case '--double-prev':
+              if (!(i === 0 || cloneArr[i - 1] === empty)) {
+                resArr.push(cloneArr[i - 1]);
+              }
+              break; 
+
+          case empty:
+              continue;
+      }
+    }
+  };
+  return resArr;
+
+
 };
