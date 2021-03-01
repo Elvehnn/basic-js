dba1c770e2b6d278f947350bdaed7e631628dea4
Elena Shashina <elvehnn@yandex.ru>
2021-03-01 10:50:20 +0300

    fix: fixed Repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,15 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options) {
-  let newStr = [];
-  newStr[0] = str;
-  if (options.additionRepeatTimes) {
-    let newAdd = new Array(additionRepeatTimes);
-    newAdd.fill(options.addition,0,additionRepeatTimes - 1);
-    for (let i = 1; i <= options.additionRepeatTimes; i++) {
-      newAdd.push(options.Separator)
+module.exports = function repeater(str, { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' }) {
+  str = String(str);
+  separator = String(separator);
+  addition = String(addition);
+  additionSeparator = String(additionSeparator);
+
+  let newStr = '';
+  let strAdd = '';
+  
+  if (addition) {
+    for (let i = 1; i <= additionRepeatTimes; i++) {
+      strAdd += `${addition}${additionSeparator}`;
+      // console.log(strAdd);
+    };
+    
+    strAdd = strAdd.slice(0,-additionSeparator.length);
+
+    for (let j = 1; j <= repeatTimes; j++){
+      newStr += `${str}${strAdd}${separator}`;
+    };
+    // console.log(newStr);
+  
+  } else {
+    for (let j = 1; j <= repeatTimes; j++){
+      newStr += `${str}${separator}`;
     }
+    // console.log(newStr);
   }
+  
+  newStr = newStr.slice(0,-separator.length);
+  // console.log(newStr);
+  return (newStr);
   
 };
   
